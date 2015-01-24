98e48af546334af84de3a80bc6e4d945add52af0
kevinkuanchenxiong <xiongkuanchen@gmail.com>
2015-01-24 04:09:49 +0000

    updated HelloWorld

--- lab01-Helloworld/HelloWorld.java
+++ lab01-Helloworld/HelloWorld.java
@@ -1,12 +1,21 @@
+//Kevin Xiong
+//FIRST LAB
+//Hello World Java Program
+
+//Define a class
 public class HelloWorld{
-        public static void main(String[] args){
-            System.out.println("##########################");
-            System.out.println("##########################");
-            System.out.println("#                        #");
-            System.out.println("#   Hello World 2015!!   #");
-            System.out.println("#                        #");
-            System.out.println("##########################");
-            System.out.println("##########################");
-            System.out.println("test");
+
+//add main method    
+    public static void main(String[] args){
+
+//print the statement with box around it
+        System.out.println("##########################");
+        System.out.println("##########################");
+        System.out.println("#                        #");
+        System.out.println("#   Hello World 2015!!   #");
+        System.out.println("#                        #");
+        System.out.println("##########################");
+        System.out.println("##########################");
+        System.out.println("test");
         }
 }
