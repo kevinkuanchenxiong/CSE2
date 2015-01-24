6a07e649359ad321beed30902945d40ab30aca09
kevinkuanchenxiong <xiongkuanchen@gmail.com>
2015-01-24 04:39:47 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,23 @@
+//Kevin Xiong
+//FIRST HOMEWORK
+//Welcome Class Java Program
+
+//Define a class
+public class WelcomeClass{
+
+//add main method    
+    public static void main(String[] args){
+
+//print the statement with box around it
+        System.out.println("  -----------");
+        System.out.println("  | WELCOME |");
+        System.out.println("  -----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("<-K--U--X--2--1--4->");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("I am an exchange student from China.");
+        System.out.println("I am a senior from Southeast University.");
+        }
+}
